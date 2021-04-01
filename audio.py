7599c691d01b974bd470cbf8187446d6e5ae849a
Tora Sirkka <tora@torasirkka.fi>
2021-03-31 19:38:54 -0700

    Improve readability Changed some variable names clarified docstrings.

--- audio.py
+++ audio.py
@@ -27,8 +27,14 @@ def frequency_dict(octave: int) -> Dict[str, float]:
 
 
 def frequencies(scale: List[str]) -> List[float]:
-    """Translates a scale to a list of frequencies."""
+    """Translates a scale to a list of frequencies.
 
+    In order to make sure the right frequency of the note is played, I keep track of
+    the last, the current frequency, and the current octave. A scale is a sequence of
+    notes that increase in frequency. Hence, if the calculated note has lower frequency,
+    it is time to go up a scale.
+    A scale is typically played up and down, which is why the frequencies list is extended to
+    include the frequencies 'backwards'."""
     frequencies = [0.0]
     octave_index = LOWEST_OCTAVE
     freq_index = 0
@@ -47,40 +53,37 @@ def frequencies(scale: List[str]) -> List[float]:
     return frequencies
 
 
-# get timesteps for each sample, T is note duration in seconds
 def timesteps(event_duration: float, sample_rate: float) -> np.ndarray:
-    """Creates an array of discrete timesteps."""
-
+    """Creates an array of discrete timesteps based on event duration (time[s]) and sample_rate [Hz]."""
     return np.linspace(0, event_duration, int(event_duration * sample_rate), False)
 
 
-def note_arrays(frequencies: List[float]) -> List[np.ndarray]:
-    """Generate sine wave notes."""
-    steps = timesteps(event_duration=EVENT_DURATION, sample_rate=SAMPLE_RATE)
-    notes: List[np.ndarray] = []
+def wave_arrays(frequencies: List[float]) -> List[np.ndarray]:
+    """Translates frequencies (floats) to discretized sine waves (arrays) corresponding to the notes."""
+    discretized_time = timesteps(event_duration=EVENT_DURATION, sample_rate=SAMPLE_RATE)
+    waves: List[np.ndarray] = []
     for f in frequencies:
-        notes += [np.sin(f * 2 * np.pi * steps)]
-    return notes
+        waves += [np.sin(f * 2 * np.pi * discretized_time)]
+
+    return waves
 
 
-def play_notes(note_array: np.ndarray):
-    """plays a note"""
-    # concatenate notes
-    audio = np.hstack((note_array))
-    # normalize to 16-bit range
+def play_notes(wave_array: List[np.ndarray]):
+    """A list of sinus wave arrays are played one after another.
+    To accomplish this, the arrays are concatenated, converted to 16-bit data before being played
+    by the sa module. The program waits for playback to finish before exiting."""
+
+    audio = np.hstack((wave_array))
     audio *= 32767 / np.max(np.abs(audio))
-    # convert to 16-bit data
     audio = audio.astype(np.int16)
-    # start playback
     play_obj = sa.play_buffer(audio, 1, 2, SAMPLE_RATE)
-    # wait for playback to finish before exiting
     play_obj.wait_done()
 
 
-# flow that funnels scale through the work:
 def play_scale(scale: List[str], mode_number: int):
+    """Support function that funnels a scale and mode number all the way to being played"""
     freqs = frequencies(scale)
-    notes = note_arrays(freqs)
+    notes = wave_arrays(freqs)
     play_notes(notes)
 
 
